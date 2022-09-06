767d7c22d24c93d91a0ed7799cfa633701fc4335
MK-Khan123 <mehnazkhan231@gmail.com>
2022-09-06 10:32:04 +0600

    Designed the Navbar

--- src/components/nav/TheNavigation.vue
+++ src/components/nav/TheNavigation.vue
@@ -1,18 +1,38 @@
 <template>
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <router-link to="/teams">Teams</router-link>
-          </li>
-          <li>
-            <router-link to="/users">Users</router-link>
-          </li>
-        </ul>
-      </nav>
+    <header class="w-full h-20 bg-stone-300">
+        <nav class="h-full">
+            <ul class="m-0 p-0 h-full list-none flex justify-center items-center">
+                <li class="mx-8">
+                    <router-link to="/teams">Home</router-link>
+                </li>
+                <li class="mx-8">
+                    <router-link to="/teams">Teams</router-link>
+                </li>
+                <li class="mx-8">
+                    <router-link to="/users">Users</router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
-  </template>
+</template>
   
-  <style>
-  
-  </style>
+<style scoped>
+a {
+    text-decoration: none;
+    background: transparent;
+    border: 1px solid transparent;
+    cursor: pointer;
+    color: black;
+    border: 1px solid black;
+    padding: 0.5rem 1.5rem;
+    display: inline-block;
+}
+
+a:hover,
+a:active,
+a.active {
+    color: #f1a80a;
+    border-color: #f1a80a;
+    background-color: rgb(41 37 36);
+}
+</style>
