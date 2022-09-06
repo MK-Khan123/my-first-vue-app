2222c4d37e3580d5e83e6bbf0074c397d6e756a2
MK-Khan123 <mehnazkhan231@gmail.com>
2022-09-06 10:16:02 +0600

    Created the navigation part

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <the-navigation></the-navigation>
 </template>
 
+<script>
+import TheNavigation from './components/nav/TheNavigation.vue';
+
+export default {
+  components: {
+    TheNavigation,
+  },
+  data() {
+    return {
+      teams: [
+        { id: 't1', name: 'Engineering Team', members: ['u1', 'u2'] },
+        { id: 't2', name: 'Design Team', members: ['u3', 'u4'] },
+        { id: 't3', name: 'Business Development', members: ['u5', 'u6'] },
+      ],
+      users: [
+        { id: 'u1', fullName: 'Alex Jones', role: 'Engineer' },
+        { id: 'u2', fullName: 'Thomas Holland', role: 'Engineer' },
+        { id: 'u3', fullName: 'James Engelsman', role: 'Designer' },
+        { id: 'u4', fullName: 'Jay Smith', role: 'Designer' },
+        { id: 'u5', fullName: 'Mike Ross', role: 'Business Development' },
+        { id: 'u6', fullName: 'Louis Litt', role: 'Business Development' },
+      ],
+    };
+  },
+};
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/nav/TheNavigation.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <router-link to="/teams">Teams</router-link>
+          </li>
+          <li>
+            <router-link to="/users">Users</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <style>
+  
+  </style>
